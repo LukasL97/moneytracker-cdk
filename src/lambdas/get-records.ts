74c40c3c636b95ad2f1ce62039d530d88e651c77
Lukas Lehmann <lukas.lehmann97@gmail.com>
2022-07-31 16:58:47 +0200

    fix DB scan in get-records

--- src/lambdas/get-records.ts
+++ src/lambdas/get-records.ts
@@ -1,6 +1,5 @@
 import {APIGatewayProxyEventV2, APIGatewayProxyResultV2} from 'aws-lambda'
 import {StatusCodes} from 'http-status-codes'
-import {Record} from '../model/Record'
 import {DynamoDB} from 'aws-sdk'
 
 const db = new DynamoDB.DocumentClient()
@@ -13,14 +12,11 @@ export async function getRecords(event: APIGatewayProxyEventV2): Promise<APIGate
       statusCode: StatusCodes.BAD_REQUEST
     }
   } else {
-    return  db.scan({
+    return db.scan({
       TableName: process.env.DYNAMODB_TABLE!!,
-      ExpressionAttributeValues: {
-        ':user': {
-          S: user
-        }
-      },
-      FilterExpression: 'user = :user'
+      FilterExpression: '#user = :user',
+      ExpressionAttributeNames: { '#user': 'user' },
+      ExpressionAttributeValues: { ':user': user }
     }).promise().then(result => {
       return {
         body: JSON.stringify(result.Items),
