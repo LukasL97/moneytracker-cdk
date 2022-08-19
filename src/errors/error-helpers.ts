132cf85f31d1bb62891acb604975c5524acac6a7
Lukas Lehmann <lukas.lehmann97@gmail.com>
2022-08-19 15:53:31 +0200

    log errors in lambdas

--- src/errors/error-helpers.ts
+++ src/errors/error-helpers.ts
@@ -4,6 +4,7 @@ import {StatusCodes} from 'http-status-codes'
 
 export async function withErrorMapping(f: () => Promise<APIGatewayProxyResultV2>): Promise<APIGatewayProxyResultV2> {
   return f().catch(error => {
+    console.error(error)
     if (error instanceof BaseError) {
       return error.toResponse()
     } else {
